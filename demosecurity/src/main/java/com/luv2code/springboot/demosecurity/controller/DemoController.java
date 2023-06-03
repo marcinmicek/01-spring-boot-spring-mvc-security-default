b2dffded87a7bf7527c046dad5ed406ccbcf77f7
Marcin Micek <marcin.micek@gmail.com>
2023-06-03 13:01:45 +0200

    Spring Boot 3 - Spring MVC Security - Restrict URLs based on roles.

--- demosecurity/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ demosecurity/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -11,4 +11,26 @@ public class DemoController {
 
         return "home";
     }
+
+    // add a request mapping for /leaders
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+
+        return "leaders";
+    }
+
+    // add a request mapping for /systems
+
+    @GetMapping("/systems")
+    public String showSystems() {
+
+        return "systems";
+    }
 }
+
+
+
+
+
+
